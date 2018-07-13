d4fc58784304b77ead8769805a954468ceeb225f
Rivan <rivan_ghafara@yahoo.com>
2018-07-13 09:38:57 +0700

    Persiapan Rombak

--- 2. SupportVector.py
+++ 2. SupportVector.py
@@ -1,6 +1,8 @@
 import csv
 import numpy as np
 import math
+from sklearn.model_selection import train_test_split
+
 
 def ReadCSV(_filename):
     
@@ -27,30 +29,58 @@ def ReadCSV(_filename):
     return sentimentList, valueOfSentiment
 
 def HessianMatrixFunction(_class_1, _class_2, _matrixItem, _lambda):
-    result = (_class_1 * _class_2) * _matrixItem + (math.pow(_lambda, 2))
+    _result = (_class_1 * _class_2) * _matrixItem + (math.pow(_lambda, 2))
 
-    return result
+    return _result
 
-def MeasuringError(_matriksHessian, _alpha):
-    print("Function is not done yet")
-
+def MeasuringAlpha(_matriksHessian, _alpha, _gamma, C):
+    
     # Mencari nilai error data ke-i
-    # Aplha belum diketahui diambil dari mana
-    # Di paper Aplha di set 0.5
-    # Output dari nilai error tidak berupa matrix
-    # Nilai Error = Sigma j == 1 hingga l (alpha ke-j * setiap hessian matrix)
+    # Alpha di inisialisasi = 0
+    # Lakulan perulangan hingga didapatkan semua nilai
+    # Di paper Alpha yang didapatkan = 0.5
+    # Output dari nilai error berupa vektor sebanyak tanggapan
+    
+    _listOfError = []
+    _listOfDeltaAlpha = []
+    _newAlpha = []
+    temp = 0
+    x = 0
+
+    print("Ini Hessian Matrix :", _matriksHessian)
+
+    for _, row in enumerate(_matriksHessian):
+        for _, var in enumerate(row):
+            temp = temp + (_alpha * var)
+        _listOfError.append(temp)
+    print("List Nilai Error", _listOfError)
 
 
-def KernelRBF(_sentimentList, _valueOfSentiment):
+    for _, var in enumerate(_listOfError):    
+        equ1 = max(_gamma * (1 - var), -_alpha)
+        equ2 = min(equ1, (C-_alpha))
+        _listOfDeltaAlpha.append(equ2)
+
+    print("List Deltha Alpha :", _listOfDeltaAlpha)
+
+    for i in range(len(_listOfDeltaAlpha)):
+        _newAlpha.append(_alpha + _listOfDeltaAlpha[i])
+
+    print(_newAlpha)
+
+
+
+def LinearKernel(_xTrain, y_train, _lambda):
     
     newValueOfMatrix = []
-    transposeOfSentiment = _sentimentList.transpose()    
-    matrixGenerate = _sentimentList.dot(transposeOfSentiment)
+    _xTrainTranpose = _xTrain.transpose()    
+    matrixGenerate = _xTrain.dot(_xTrainTranpose)
     
     for i in range(len(matrixGenerate)):
         tempList = []
         for j, var in enumerate(matrixGenerate[i]):
-            var = HessianMatrixFunction(_valueOfSentiment[i], _valueOfSentiment[j], var, 0.5)
+            var = HessianMatrixFunction(y_train[i], y_train[j], var, _lambda)
+            # print('HessianMatrixFunction(%d, %d, %d, 0.5)' % (y_train[i], y_train[j], var))
             tempList.append(var)
         newValueOfMatrix.insert(i, tempList)
 
@@ -59,11 +89,25 @@ def KernelRBF(_sentimentList, _valueOfSentiment):
     return newValueOfMatrix
 
 def main():
-    
-    vectorOfOpinion, valueOfSentiment = ReadCSV('Result/TF_IDF.csv')
-    hessianMatrix = KernelRBF(vectorOfOpinion, valueOfSentiment)
+    # Inisialisasi 
+    filename = 'Result/TF_IDF.csv'
+    splitting = 0.1
+
+    setAlpha = 0
+    setGamma = 0.5
+    setLambda = 0.5
+    setEpisolon = 0.001
+    C = 1
+
+
+    vectorOfOpinion, valueOfSentiment = ReadCSV(filename)
+    x_train, x_test, y_train, y_test = train_test_split(vectorOfOpinion, valueOfSentiment, test_size=splitting, random_state=0)
+    hessianMatrix = LinearKernel(x_train, y_train, setLambda)
+    # hessianMatrix = LinearKernel(vectorOfOpinion, valueOfSentiment)
+    MeasuringAlpha(hessianMatrix, setAlpha, setGamma, C)
+    # print(hessianMatrix.shape)
     # print(valueOfSentiment)
 
-    print(hessianMatrix)
+    # print(hessianMatrix)
 
 main()
---
+++ Test002.py
@@ -0,0 +1,5 @@
+a = [1 ,2]
+b = [3, 4]
+
+c = [x + y for x, y in zip(a, b)]
+print(c)
